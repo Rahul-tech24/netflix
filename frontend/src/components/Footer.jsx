3fa3dc2c732cf55e297395a8700375a623f4fe12
Rahul Rajput <rahulrajput808160@gmail.com>
2025-10-23 13:24:23 +0530

    code updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,8 @@
 const Footer = () => {
+
+
+
+
 	return (
 		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
 			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
@@ -27,3 +31,46 @@ const Footer = () => {
 	);
 };
 export default Footer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
